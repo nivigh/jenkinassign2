e96238f7d3b65f603855c1e9677d5e5ba962ee0c
Ubuntu <nivedita@niveditalinux.bre03aww3piurpzcckxqnrawob.cx.internal.cloudapp.net>
2023-02-16 09:43:47 +0000

    This is Java file written & committed in Local

--- jenkin2.java
+++ jenkin2.java
@@ -1,6 +1,6 @@
 public class jenkin2{
      public static void main(String[] args)
        {
-            System.out.println("Hellooo Nivi!");
+            System.out.println("Hellooo folks!");
        }
 }
